0c40af7be88eed9f2fa85fe0e4d6c585717524a7
Buni48 <dago24@posteo.de>
2022-10-07 23:07:15 +0200

    more prettier

--- vite-site.config.js
+++ vite-site.config.js
@@ -1,18 +1,16 @@
-import { fileURLToPath, URL } from 'url';
+import {fileURLToPath, URL} from 'url';
 import vue from '@vitejs/plugin-vue';
-import path from "path";
+import path from 'path';
 
 export default {
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  plugins: [
-    vue({}),
-  ],
+  plugins: [vue({})],
   build: {
-    outDir: 'dist-site'
-  }
+    outDir: 'dist-site',
+  },
 };
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
-import { fileURLToPath, URL } from 'url';
+import {fileURLToPath, URL} from 'url';
 import vue from '@vitejs/plugin-vue';
-import path from "path";
+import path from 'path';
 
 export default {
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  plugins: [
-    vue({}),
-  ],
+  plugins: [vue({})],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/components/TwitterTimeline.vue"),
-      name: "vue-twitter-timeline",
+      entry: path.resolve(__dirname, 'src/components/TwitterTimeline.vue'),
+      name: 'vue-twitter-timeline',
     },
-  }
+  },
 };
